885a57788e011687d84f8946c195dd84c454a732
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-06-30 14:59:20 +1300

    setting user router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,26 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+import userRoutes from './routes/userRoutes'
 
 const app = express();
-const PORT = 5000;
 
-app.get('/', (req: Request, res: Response) => {
-     res.send("Hello Vishal")
+const prisma = new PrismaClient();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/test-db', async (req: Request, res: Response) => {
+     try {
+          await prisma.$connect();
+          res.status(200).json({ message: "Database is connected"})
+     } catch (error) {
+          res.status(500).json({ message: "Database connection failed", error})          
+     }
 })
 
+app.use('/api/user', userRoutes);
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+export { prisma };
