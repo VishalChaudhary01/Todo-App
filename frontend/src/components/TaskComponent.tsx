f42a1b5b81e21d4007d8117d421540ae8178dfc9
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-07-08 03:54:30 +1300

    single task component

---
+++ frontend/src/components/TaskComponent.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { red } from "@mui/material/colors";
+
+export const SingleTask: React.FC<TaskProps> = ({
+  id,
+  title,
+  description,
+  done,
+  handleDeleteTask,
+  handleUpdateTask,
+}) => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className={`py-2 px-3 my-3 w-1/2 block max-w-sm items-center border border-black rounded-lg shadow hover:bg-green-100" ${done ? "bg-green-300": "" }`}>
+        <div className="flex justify-between items-center">
+            <div>
+              <h1 className="font-bold">{title}</h1>
+              <h4>{description}</h4>
+            </div>
+            <div>
+              <button onClick={() => handleUpdateTask(id)} className="mx-2">
+                <TaskAltIcon color="success" />
+              </button>
+              <button onClick={() => handleDeleteTask(id)} className="mx-2">
+                <DeleteIcon sx={{ color: red[600] }} />
+              </button>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
