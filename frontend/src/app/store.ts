f02ebdb3411acff5e5b913b5d3afb355121d355f
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-07-08 03:45:29 +1300

    redux store

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,48 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { thunk } from "redux-thunk";
+
+import { 
+     signinReducer, 
+     signupReducer 
+} from "../reducers/userReducer";
+
+import {
+  createTaskReducer,
+  deleteTaskReducer,
+  getTasksReducer,
+  updateTaskReducer,
+} from "../reducers/taskReducer";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+const reducer = combineReducers({
+  userSignin: signinReducer,
+  userSignup: signupReducer,
+  createTask: createTaskReducer,
+  updateTask: updateTaskReducer,
+  deleteTask: deleteTaskReducer,
+  allTasks: getTasksReducer,
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+export type RootState = ReturnType<typeof reducer>
+
+export const makeStore = (prelodadState?: Partial<RootState>) => {
+  const store = configureStore({
+    reducer: reducer,
+    middleware: getDefaultMiddleware => {
+      return getDefaultMiddleware().concat(middleware)
+    },
+    preloadedState: initialState,
+  })
+  setupListeners(store.dispatch)
+  return store
+}
+
+export const store = makeStore()
+export type AppStore = typeof store;
+
+export type AppDispatch = AppStore['dispatch']
+
