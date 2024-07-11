23d5e0fa4dcbfe64064aba6e91ddcebc31084142
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-07-11 19:15:18 +1300

    fix private route

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -2,26 +2,45 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { HomePage } from "../pages/HomePage";
 import { SignupPage } from "../pages/SignupPage";
+import { SigninPage } from "../pages/SigninPage";
 import { AddTaskPage } from "../pages/AddTaskPage";
+import { PrivateRoute } from "../components/PrivateRoute";
 
 
-export const router = createBrowserRouter([
-     {
-          path: "/",
-          element: <App />,
-          children: [
-               {
-                    path: "/",
-                    element: <HomePage />
-               },
-               {
-                    path: "/add-task",
-                    element: <AddTaskPage />
-               },
-               {
-                    path: "/signup",
-                    element: <SignupPage />
-               }
-          ]
-     }
-]);
+export const router = createBrowserRouter(
+     [
+          {
+               path: "/",
+               element: <App />,
+               children: [
+                    {
+                         path: "/",
+                         element: <PrivateRoute />,
+                         children: [
+                              {
+                                   path: "/",
+                                   element: <HomePage />
+                              },
+                              {
+                                   path: "/add-task",
+                                   element: <AddTaskPage />
+                              },
+                         ]
+
+                    },
+                    {
+                         path: "/signin",
+                         element: <SigninPage />
+                    },
+                    {
+                         path: "/signup",
+                         element: <SignupPage />
+                    },
+                    {
+                         path: '*',
+                         element: <p className="font-blod 2xl">404 Error - Nothing here...</p>
+                    }
+               ]
+          }
+     ]
+);
