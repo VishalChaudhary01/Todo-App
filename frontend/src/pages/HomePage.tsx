775e85fcf57fdb961d367b51175bcb5d518e6f7c
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-07-11 19:19:20 +1300

    update home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,22 +1,10 @@
-import { useAppSelector } from "../app/hooks";
+import * as React from 'react';
 import Tasks from "../components/Tasks";
-import { SigninPage } from "./SigninPage";
 
-export const HomePage = () => {
-  const { userInfo } = useAppSelector((state) => state.userSignin);
-
+export const HomePage: React.FC = () => {
   return (
     <>
-      {userInfo ? (
-        <Tasks />
-      ) : (
-        <div>
-          <h2 className="text-4xl font-bold text-center py-3">
-            Please sign in to add your tasks!
-          </h2>
-          <SigninPage />
-        </div>
-      )}
+      <Tasks />
     </>
   );
 };
